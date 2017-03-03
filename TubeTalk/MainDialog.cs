042d465e32f95a259fabe60dd1b71e9aad957267
Paul DeCarlo <pdecarlo@microsoft.com>
2017-03-03 09:27:11 -0800

    Add Youtube Search

--- TubeTalk/MainDialog.cs
+++ TubeTalk/MainDialog.cs
@@ -7,12 +7,17 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Connector;
 using System.Text.RegularExpressions;
 using System.Net.Http;
+using Google.Apis.YouTube.v3;
+using Google.Apis.Services;
+using System.Web.Configuration;
+using System.Configuration;
 
 namespace TubeTalk
 {
     [Serializable]
     public class MainDialog : IDialog<object>
     {
+
         static HttpClient client = new HttpClient();
         public async Task StartAsync(IDialogContext context)
         {
@@ -23,8 +28,8 @@ namespace TubeTalk
         {
             var message = await argument;
             // Put correct regex here 
-            //var regex = new Regex("http(?:s?):\\/\\/(?:www\\.)?youtu(?:be\\.com\\/watch\\?v=|\\.be\\/)([\\w\\-\\_]*)(&(amp;)?‌​[\\w\\?‌​=]*)?");
             var regex = new Regex("^http");
+            //var regex = new Regex(@"http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?");
             if (regex.Match(message.Text).Success)
             {
                 try
@@ -41,9 +46,22 @@ namespace TubeTalk
             }
             else
             {
+                Activity replyToConversation = context.MakeMessage() as Activity;
+                replyToConversation.Attachments = new List<Attachment>();
+
                 // Non link - query Youtube here
-                GetYoutubeVideos(message.Text);
-                await context.PostAsync("Please paste a URL for the Youtube content you desire.");
+        
+                var herocards = await GetYoutubeVideos(message.Text);
+                
+                foreach(var heroCard in herocards)
+                {
+                    Attachment plAttachment = heroCard.ToAttachment();
+                    replyToConversation.Attachments.Add(plAttachment);
+                }
+
+                await context.PostAsync(replyToConversation);
+
+                //await context.PostAsync("Please paste a URL for the Youtube content you desire.");
                 context.Wait(MessageReceivedAsync);
             }
         }
@@ -80,33 +98,59 @@ namespace TubeTalk
         /// Hero Cards will appear in a carosel and look pretty
         /// </summary>
         /// <param name="text"></param>
-        private List<HeroCard> GetYoutubeVideos(string text)
+        private async Task<List<HeroCard>> GetYoutubeVideos(string text)
         {
             List<HeroCard> vidcards = new List<HeroCard>();
 
+
+            string youtubePrefix = "https://www.youtube.com/watch?v=";
+
             // query youtube api
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+
+            {
+
+                ApiKey = WebConfigurationManager.AppSettings["YoutubeKey"],
+
+                ApplicationName = "Searchler"
+
+            });
+
+
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+
+            searchListRequest.MaxResults = 3;
 
             // get back title of the video, thumbnail, desctiption, url
-            
 
-            foreach (Object x in YoutubePostResult)
+            searchListRequest.Q = text;
+
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+
+
+            foreach (var searchResult in searchListResponse.Items)
             {
-                
+
+                var downloadURL = await CallYoutubeExtractorFunction(youtubePrefix + searchResult.Id.VideoId);
+
+                downloadURL = downloadURL.Remove(0,13);
+
                 HeroCard vidHeroCard = new HeroCard()
                 {
-                    Title = videoname,
-                    Subtitle = videodescription,
+                    Title = searchResult.Snippet.Title,
+                    Subtitle = searchResult.Snippet.Description,
                     Images = new List<CardImage>()
                         {
-                            new CardImage() { Url = videoThumbnail }
+                            new CardImage() { Url = searchResult.Snippet.Thumbnails.Medium.Url }
                         },
                     Buttons = new List<CardAction>()
                         {
                             new CardAction()
                             {
-                                Title = "More details",
-                                Type = ActionTypes.OpenUrl,
-                                Value = videourl
+                                Title = "Open Download Link",
+                                Type = ActionTypes.DownloadFile,
+                                Value = downloadURL
                             }
                         }
                 };
